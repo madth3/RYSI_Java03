96c4d5a54254f767ad249b49ac74d0fb16bf6894
Julio Aguilar <madth3.dev@gmail.com>
2016-02-06 11:06:43 -0600

    Ajustes y correcciones varios.

--- HolaWeb/src/main/java/rysi/web/holaweb/Aplicacion.java
+++ HolaWeb/src/main/java/rysi/web/holaweb/Aplicacion.java
@@ -1,9 +1,12 @@
 package rysi.web.holaweb;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  *
@@ -16,6 +19,11 @@ public class Aplicacion extends SpringBootServletInitializer {
     protected SpringApplicationBuilder
             configure(SpringApplicationBuilder builder) {
         return builder.sources(Aplicacion.class);
+    }
+    
+    @Bean(name="entityManagerFactory")
+    public EntityManagerFactory construirEMF() {
+        return Persistence.createEntityManagerFactory("HolaWebPU");
     }
 
     public static void main(String[] args) {
---
+++ HolaWeb/src/main/java/rysi/web/holaweb/oad/ClubOad.java
@@ -0,0 +1,12 @@
+package rysi.web.holaweb.oad;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rysi.web.holaweb.entidades.Club;
+
+/**
+ *
+ * @author jaguilar
+ */
+public interface ClubOad extends JpaRepository<Club, Integer>{
+    
+}
