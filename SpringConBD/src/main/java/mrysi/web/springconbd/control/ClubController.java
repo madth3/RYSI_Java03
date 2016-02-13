685ead905459d448b52b1e4fece8ba3bed2b89ff
Julio Aguilar <madth3.dev@gmail.com>
2016-02-13 17:26:03 -0600

    Ultima version

--- SpringConBD/src/main/java/mrysi/web/springconbd/control/ClubController.java
+++ SpringConBD/src/main/java/mrysi/web/springconbd/control/ClubController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import mrysi.web.springconbd.entidades.Club;
 import mrysi.web.springconbd.oad.ClubOad;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,11 @@ public class ClubController {
     @RequestMapping(value="", method=RequestMethod.POST)
     public Club agregar(@RequestBody Club club) {
         return clubOad.save(club);
+    }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public void borrar(@PathVariable Integer id) {
+        clubOad.delete(id);
     }
     
 }
--- SpringConBD/src/main/webapp/js/indexcontroller.js
+++ SpringConBD/src/main/webapp/js/indexcontroller.js
@@ -3,11 +3,72 @@ var AppClubs = angular.module('AppClubs',[]);
 
 AppClubs.controller('mostrarClubs', function($scope, $http){
     
-    $scope.saludo = "Hola mundo";
+    $scope.cargarClubParaEdicion = function(idClub) {
+        for (var k = 0; k < $scope.listaCompletaDeClubs.length; k++) {
+            if ($scope.listaCompletaDeClubs[k].id === idClub) {
+                $scope.clubActual = $scope.listaCompletaDeClubs[k];
+            }
+        }
+    }
+
+    $scope.cargarClubs = function() {
+        // Llamada a URL "/clubs" de Spring con callback
+        $http.get('clubs')   
+                .success(function(data, status){
+                    $scope.listaCompletaDeClubs = data;
+                });
+    };
     
-    // Llamada a URL "/clubs" de Spring con callback
-    $http.get('clubs')   
-            .success(function(data, status){
-                $scope.listaCompletaDeClubs = data;
-            });
+    $scope.guardarClub = function(){
+        $scope.estaGuardando = true;
+        if (typeof $scope.clubActual.id != 'undefined') {
+            $http.put('clubs', $scope.clubActual)
+                    .success(function(data, status) {
+                        $scope.clubActual = {};
+                        $scope.cargarClubs();
+                        $scope.estaGuardando = false;
+                    })
+                    .error(function(data, status) {
+                        console.log('ERROR:', data);
+                        $scope.estaGuardando = false;
+                    });            
+        }
+        else {
+            $http.post('clubs', $scope.clubActual)
+                    .success(function(data, status) {
+                        //$scope.listaCompletaDeClubs.push($scope.nuevoClub);
+                        $scope.clubActual = {};
+                        $scope.cargarClubs();
+                        $scope.estaGuardando = false;
+                    })
+                    .error(function(data, status) {
+                        console.log('ERROR:', data);
+                        $scope.estaGuardando = false;
+                    });
+        }
+    };
+    
+    $scope.eliminarClub = function(idClub) {
+        $http.delete('clubs/' + idClub)
+                .success(function(data, status) {
+                    $scope.cargarClubs();
+                })
+                .error(function(data, status) {
+                    console.log('ERROR:', data);
+                });
+    }
+    
+
+    $scope.saludo = "Hola mundo";
+    $scope.clubActual = {};
+    $scope.cargarClubs();
+    
 });
+
+
+
+
+
+
+
+
